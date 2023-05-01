d0562cab346b29e26b88c396398d4eb0ca7e57a3
Vladislav Koshkarev <dddvvv.dv@gmail.com>
2023-05-01 22:56:13 +0300

    feat: add rus module

--- keys.js
+++ keys.js
@@ -1,130 +1,391 @@
-const eng = [{ key: '`', code: 'Backquote' },
-  { key: '1', code: 'Digit1' },
-  { key: '2', code: 'Digit2' },
-  { key: '3', code: 'Digit3' },
-  { key: '4', code: 'Digit4' },
-  { key: '5', code: 'Digit5' },
-  { key: '6', code: 'Digit6' },
-  { key: '7', code: 'Digit7' },
-  { key: '8', code: 'Digit8' },
-  { key: '9', code: 'Digit9' },
-  { key: '0', code: 'Digit0' },
-  { key: '-', code: 'Minus' },
-  { key: '=', code: 'Equal' },
-  { key: 'Backspace', code: 'Backspace' },
-  { key: 'Tab', code: 'Tab' },
-  { key: 'q', code: 'KeyQ' },
-  { key: 'w', code: 'KeyW' },
-  { key: 'e', code: 'KeyE' },
-  { key: 'r', code: 'KeyR' },
-  { key: 't', code: 'KeyT' },
-  { key: 'y', code: 'KeyY' },
-  { key: 'u', code: 'KeyU' },
-  { key: 'i', code: 'KeyI' },
-  { key: 'o', code: 'KeyO' },
-  { key: 'p', code: 'KeyP' },
-  { key: '[', code: 'BracketLeft' },
-  { key: ']', code: 'BracketRight ' },
-  { key: '\\', code: 'Backslash' },
-  { key: 'Delete', code: 'Delete' },
-  { key: 'CapsLock', code: 'CapsLock' },
-  { key: 'a', code: 'KeyA' },
-  { key: 's', code: 'KeyS' },
-  { key: 'd', code: 'KeyD' },
-  { key: 'f', code: 'KeyF' },
-  { key: 'g', code: 'KeyG' },
-  { key: 'h', code: 'KeyH' },
-  { key: 'j', code: 'KeyJ' },
-  { key: 'k', code: 'KeyK' },
-  { key: 'l', code: 'KeyL' },
-  { key: ';', code: 'Semicolon' },
-  { key: "'", code: 'Quote' },
-  { key: 'Enter', code: 'Enter' },
-  { key: 'Shift', code: 'ShiftLeft' },
-  { key: 'z', code: 'KeyZ' },
-  { key: 'x', code: 'KeyX' },
-  { key: 'c', code: 'KeyC' },
-  { key: 'v', code: 'KeyV' },
-  { key: 'b', code: 'KeyB' },
-  { key: 'n', code: 'KeyN' },
-  { key: 'm', code: 'KeyM' },
-  { key: ',', code: 'Comma' },
-  { key: '.', code: 'Period' },
-  { key: '/', code: 'Slash' },
-  { key: '↑', code: 'ArrowUp' },
-  { key: 'Shift', code: 'ShiftRight' },
-  { key: 'Control', code: 'ControlLeft' },
-  { key: 'Alt', code: 'AltLeft' },
-  { key: 'Meta', code: 'MetaLeft' },
-  { key: ' ', code: 'Space' },
-  { key: 'Meta', code: 'MetaRight' },
-  { key: 'Alt', code: 'AltRight' },
-  { key: '←', code: 'ArrowLeft' },
-  { key: '↓', code: 'ArrowDown' },
-  { key: '→', code: 'ArrowRight' }];
-const rus = [{ key: 'ё', code: 'Backquote' },
-  { key: '1', code: 'Digit1' },
-  { key: '2', code: 'Digit2' },
-  { key: '3', code: 'Digit3' },
-  { key: '4', code: 'Digit4' },
-  { key: '5', code: 'Digit5' },
-  { key: '6', code: 'Digit6' },
-  { key: '7', code: 'Digit7' },
-  { key: '8', code: 'Digit8' },
-  { key: '9', code: 'Digit9' },
-  { key: '0', code: 'Digit0' },
-  { key: '-', code: 'Minus' },
-  { key: '=', code: 'Equal' },
-  { key: 'Backspace', code: 'Backspace' },
-  { key: 'Tab', code: 'Tab' },
-  { key: 'й', code: 'KeyQ' },
-  { key: 'ц', code: 'KeyW' },
-  { key: 'у', code: 'KeyE' },
-  { key: 'к', code: 'KeyR' },
-  { key: 'е', code: 'KeyT' },
-  { key: 'н', code: 'KeyY' },
-  { key: 'г', code: 'KeyU' },
-  { key: 'ш', code: 'KeyI' },
-  { key: 'щ', code: 'KeyO' },
-  { key: 'з', code: 'KeyP' },
-  { key: 'х', code: 'BracketLeft' },
-  { key: 'ъ', code: 'BracketRight ' },
-  { key: '\\', code: 'Backslash' },
-  { key: 'Delete', code: 'Delete' },
-  { key: 'CapsLock', code: 'CapsLock' },
-  { key: 'ф', code: 'KeyA' },
-  { key: 'ы', code: 'KeyS' },
-  { key: 'в', code: 'KeyD' },
-  { key: 'а', code: 'KeyF' },
-  { key: 'п', code: 'KeyG' },
-  { key: 'р', code: 'KeyH' },
-  { key: 'о', code: 'KeyJ' },
-  { key: 'л', code: 'KeyK' },
-  { key: 'д', code: 'KeyL' },
-  { key: 'ж', code: 'Semicolon' },
-  { key: 'э', code: 'Quote' },
-  { key: 'Enter', code: 'Enter' },
-  { key: 'Shift', code: 'ShiftLeft' },
-  { key: 'я', code: 'KeyZ' },
-  { key: 'ч', code: 'KeyX' },
-  { key: 'с', code: 'KeyC' },
-  { key: 'м', code: 'KeyV' },
-  { key: 'и', code: 'KeyB' },
-  { key: 'т', code: 'KeyN' },
-  { key: 'ь', code: 'KeyM' },
-  { key: 'б', code: 'Comma' },
-  { key: 'ю', code: 'Period' },
-  { key: '.', code: 'Slash' },
-  { key: '↑', code: 'ArrowUp' },
-  { key: 'Shift', code: 'ShiftRight' },
-  { key: 'Control', code: 'ControlLeft' },
-  { key: 'Alt', code: 'AltLeft' },
-  { key: 'Meta', code: 'MetaLeft' },
-  { key: ' ', code: 'Space' },
-  { key: 'Meta', code: 'MetaRight' },
-  { key: 'Alt', code: 'AltRight' },
-  { key: '←', code: 'ArrowLeft' },
-  { key: '↓', code: 'ArrowDown' },
-  { key: '→', code: 'ArrowRight' }];
-
+const eng = [{
+  key: '`', key_shift: '~', key_caps: '`', key_caps_shift: '~', code: 'Backquote',
+},
+{
+  key: '1', key_shift: '!', key_caps: '1', key_caps_shift: '!', code: 'Digit1',
+},
+{
+  key: '2', key_shift: '@', key_caps: '2', key_caps_shift: '@', code: 'Digit2',
+},
+{
+  key: '3', key_shift: '#', key_caps: '3', key_caps_shift: '#', code: 'Digit3',
+},
+{
+  key: '4', key_shift: '$', key_caps: '4', key_caps_shift: '$', code: 'Digit4',
+},
+{
+  key: '5', key_shift: '%', key_caps: '5', key_caps_shift: '%', code: 'Digit5',
+},
+{
+  key: '6', key_shift: '^', key_caps: '6', key_caps_shift: '^', code: 'Digit6',
+},
+{
+  key: '7', key_shift: '&', key_caps: '7', key_caps_shift: '&', code: 'Digit7',
+},
+{
+  key: '8', key_shift: '*', key_caps: '8', key_caps_shift: '8', code: 'Digit8',
+},
+{
+  key: '9', key_shift: '(', key_caps: '9', key_caps_shift: '(', code: 'Digit9',
+},
+{
+  key: '0', key_shift: ')', key_caps: '0', key_caps_shift: ')', code: 'Digit0',
+},
+{
+  key: '-', key_shift: '_', key_caps: '-', key_caps_shift: '_', code: 'Minus',
+},
+{
+  key: '=', key_shift: '+', key_caps: '=', key_caps_shift: '+', code: 'Equal',
+},
+{
+  key: 'Backspace', key_shift: 'Backspace', key_caps: 'Backspace', key_caps_shift: 'Backspace', code: 'Backspace',
+},
+{
+  key: 'Tab', key_shift: 'Tab', key_caps: 'Tab', key_caps_shift: 'Tab', code: 'Tab',
+},
+{
+  key: 'q', key_shift: 'Q', key_caps: 'Q', key_caps_shift: 'q', code: 'KeyQ',
+},
+{
+  key: 'w', key_shift: 'W', key_caps: 'W', key_caps_shift: 'w', code: 'KeyW',
+},
+{
+  key: 'e', key_shift: 'E', key_caps: 'E', key_caps_shift: 'e', code: 'KeyE',
+},
+{
+  key: 'r', key_shift: 'R', key_caps: 'R', key_caps_shift: 'r', code: 'KeyR',
+},
+{
+  key: 't', key_shift: 'T', key_caps: 'T', key_caps_shift: 't', code: 'KeyT',
+},
+{
+  key: 'y', key_shift: 'Y', key_caps: 'Y', key_caps_shift: 'y', code: 'KeyY',
+},
+{
+  key: 'u', key_shift: 'U', key_caps: 'U', key_caps_shift: 'u', code: 'KeyU',
+},
+{
+  key: 'i', key_shift: 'I', key_caps: 'I', key_caps_shift: 'i', code: 'KeyI',
+},
+{
+  key: 'o', key_shift: 'O', key_caps: 'O', key_caps_shift: 'o', code: 'KeyO',
+},
+{
+  key: 'p', key_shift: 'P', key_caps: 'P', key_caps_shift: 'p', code: 'KeyP',
+},
+{
+  key: '[', key_shift: '{', key_caps: '[', key_caps_shift: '{', code: 'BracketLeft',
+},
+{
+  key: ']', key_shift: '}', key_caps: ']', key_caps_shift: '}', code: 'BracketRight',
+},
+{
+  key: '\\', key_shift: '|', key_caps: '\\', key_caps_shift: '|', code: 'Backslash',
+},
+{
+  key: 'Delete', key_shift: 'Delete', key_caps: 'Delete', key_caps_shift: 'Delete', code: 'Delete',
+},
+{
+  key: 'CapsLock', key_shift: 'CapsLock', key_caps: 'CapsLock', key_caps_shift: 'CapsLock', code: 'CapsLock',
+},
+{
+  key: 'a', key_shift: 'A', key_caps: 'A', key_caps_shift: 'a', code: 'KeyA',
+},
+{
+  key: 's', key_shift: 'S', key_caps: 'S', key_caps_shift: 's', code: 'KeyS',
+},
+{
+  key: 'd', key_shift: 'D', key_caps: 'D', key_caps_shift: 'd', code: 'KeyD',
+},
+{
+  key: 'f', key_shift: 'F', key_caps: 'F', key_caps_shift: 'f', code: 'KeyF',
+},
+{
+  key: 'g', key_shift: 'G', key_caps: 'G', key_caps_shift: 'g', code: 'KeyG',
+},
+{
+  key: 'h', key_shift: 'H', key_caps: 'H', key_caps_shift: 'h', code: 'KeyH',
+},
+{
+  key: 'j', key_shift: 'J', key_caps: 'J', key_caps_shift: 'j', code: 'KeyJ',
+},
+{
+  key: 'k', key_shift: 'K', key_caps: 'K', key_caps_shift: 'k', code: 'KeyK',
+},
+{
+  key: 'l', key_shift: 'L', key_caps: 'L', key_caps_shift: 'l', code: 'KeyL',
+},
+{
+  key: ';', key_shift: ':', key_caps: ';', key_caps_shift: ':', code: 'Semicolon',
+},
+{
+  key: "'", key_shift: '"', key_caps: "'", key_caps_shift: '"', code: 'Quote',
+},
+{
+  key: 'Enter', key_shift: 'Enter', key_caps: 'Enter', key_caps_shift: 'Enter', code: 'Enter',
+},
+{
+  key: 'Shift', key_shift: 'Shift', key_caps: 'Shift', key_caps_shift: 'Shift', code: 'ShiftLeft',
+},
+{
+  key: 'z', key_shift: 'Z', key_caps: 'Z', key_caps_shift: 'z', code: 'KeyZ',
+},
+{
+  key: 'x', key_shift: 'X', key_caps: 'X', key_caps_shift: 'x', code: 'KeyX',
+},
+{
+  key: 'c', key_shift: 'C', key_caps: 'C', key_caps_shift: 'c', code: 'KeyC',
+},
+{
+  key: 'v', key_shift: 'V', key_caps: 'V', key_caps_shift: 'v', code: 'KeyV',
+},
+{
+  key: 'b', key_shift: 'B', key_caps: 'B', key_caps_shift: 'b', code: 'KeyB',
+},
+{
+  key: 'n', key_shift: 'N', key_caps: 'N', key_caps_shift: 'n', code: 'KeyN',
+},
+{
+  key: 'm', key_shift: 'M', key_caps: 'M', key_caps_shift: 'm', code: 'KeyM',
+},
+{
+  key: ',', key_shift: '<', key_caps: ',', key_caps_shift: '<', code: 'Comma',
+},
+{
+  key: '.', key_shift: '>', key_caps: '.', key_caps_shift: '>', code: 'Period',
+},
+{
+  key: '/', key_shift: '?', key_caps: '/', key_caps_shift: '?', code: 'Slash',
+},
+{
+  key: '↑', key_shift: '↑', key_caps: '↑', key_caps_shift: '↑', code: 'ArrowUp',
+},
+{
+  key: 'Shift', key_shift: 'Shift', key_caps: 'Shift', key_caps_shift: 'Shift', code: 'ShiftRight',
+},
+{
+  key: 'Control', key_shift: 'Control', key_caps: 'Control', key_caps_shift: 'Control', code: 'ControlLeft',
+},
+{
+  key: 'Alt', key_shift: 'Alt', key_caps: 'Alt', key_caps_shift: 'Alt', code: 'AltLeft',
+},
+{
+  key: 'Meta', key_shift: 'Meta', key_caps: 'Meta', key_caps_shift: 'Meta', code: 'MetaLeft',
+},
+{
+  key: ' ', key_shift: ' ', key_caps: ' ', key_caps_shift: ' ', code: 'Space',
+},
+{
+  key: 'Meta', key_shift: 'Meta', key_caps: 'Meta', key_caps_shift: 'Meta', code: 'MetaRight',
+},
+{
+  key: 'Alt', key_shift: 'Alt', key_caps: 'Alt', key_caps_shift: 'Alt', code: 'AltRight',
+},
+{
+  key: '←', key_shift: '←', key_caps: '←', key_caps_shift: '←', code: 'ArrowLeft',
+},
+{
+  key: '↓', key_shift: '↓', key_caps: '↓', key_caps_shift: '↓', code: 'ArrowDown',
+},
+{
+  key: '→', key_shift: '→', key_caps: '→', key_caps_shift: '→', code: 'ArrowRight',
+},
+{
+  key: 'Control', key_shift: 'Control', key_caps: 'Control', key_caps_shift: 'Control', code: 'ControlRight',
+}];
+const rus = [{
+  key: 'ё', key_shift: 'Ë', key_caps: 'Ë', key_caps_shift: 'ё', code: 'Backquote',
+},
+{
+  key: '1', key_shift: '!', key_caps: '1', key_caps_shift: '!', code: 'Digit1',
+},
+{
+  key: '2', key_shift: '"', key_caps: '2', key_caps_shift: '"', code: 'Digit2',
+},
+{
+  key: '3', key_shift: '№', key_caps: '3', key_caps_shift: '№', code: 'Digit3',
+},
+{
+  key: '4', key_shift: ';', key_caps: '4', key_caps_shift: ';', code: 'Digit4',
+},
+{
+  key: '5', key_shift: '%', key_caps: '5', key_caps_shift: '%', code: 'Digit5',
+},
+{
+  key: '6', key_shift: ':', key_caps: '6', key_caps_shift: ':', code: 'Digit6',
+},
+{
+  key: '7', key_shift: '?', key_caps: '7', key_caps_shift: '?', code: 'Digit7',
+},
+{
+  key: '8', key_shift: '*', key_caps: '8', key_caps_shift: '*', code: 'Digit8',
+},
+{
+  key: '9', key_shift: '(', key_caps: '9', key_caps_shift: '(', code: 'Digit9',
+},
+{
+  key: '0', key_shift: ')', key_caps: '0', key_caps_shift: ')', code: 'Digit0',
+},
+{
+  key: '-', key_shift: '_', key_caps: '-', key_caps_shift: '_', code: 'Minus',
+},
+{
+  key: '=', key_shift: '+', key_caps: '=', key_caps_shift: '+', code: 'Equal',
+},
+{
+  key: 'Backspace', key_shift: 'Backspace', key_caps: 'Backspace', key_caps_shift: 'Backspace', code: 'Backspace',
+},
+{
+  key: 'Tab', key_shift: 'Tab', key_caps: 'Tab', key_caps_shift: 'Tab', code: 'Tab',
+},
+{
+  key: 'й', key_shift: 'Й', key_caps: 'Й', key_caps_shift: 'й', code: 'KeyQ',
+},
+{
+  key: 'ц', key_shift: 'Ц', key_caps: 'Ц', key_caps_shift: 'ц', code: 'KeyW',
+},
+{
+  key: 'у', key_shift: 'У', key_caps: 'У', key_caps_shift: 'у', code: 'KeyE',
+},
+{
+  key: 'к', key_shift: 'К', key_caps: 'К', key_caps_shift: 'к', code: 'KeyR',
+},
+{
+  key: 'е', key_shift: 'Е', key_caps: 'Е', key_caps_shift: 'е', code: 'KeyT',
+},
+{
+  key: 'н', key_shift: 'Н', key_caps: 'Н', key_caps_shift: 'н', code: 'KeyY',
+},
+{
+  key: 'г', key_shift: 'Г', key_caps: 'Г', key_caps_shift: 'г', code: 'KeyU',
+},
+{
+  key: 'ш', key_shift: 'Ш', key_caps: 'Ш', key_caps_shift: 'ш', code: 'KeyI',
+},
+{
+  key: 'щ', key_shift: 'Щ', key_caps: 'Щ', key_caps_shift: 'щ', code: 'KeyO',
+},
+{
+  key: 'з', key_shift: 'З', key_caps: 'З', key_caps_shift: 'з', code: 'KeyP',
+},
+{
+  key: 'х', key_shift: 'Х', key_caps: 'х', key_caps_shift: 'Х', code: 'BracketLeft',
+},
+{
+  key: 'ъ', key_shift: 'Ъ', key_caps: 'ъ', key_caps_shift: 'Ъ', code: 'BracketRight',
+},
+{
+  key: '\\', key_shift: '|', key_caps: '\\', key_caps_shift: '|', code: 'Backslash',
+},
+{
+  key: 'Delete', key_shift: 'Delete', key_caps: 'Delete', key_caps_shift: 'Delete', code: 'Delete',
+},
+{
+  key: 'CapsLock', key_shift: 'CapsLock', key_caps: 'CapsLock', key_caps_shift: 'CapsLock', code: 'CapsLock',
+},
+{
+  key: 'ф', key_shift: 'Ф', key_caps: 'Ф', key_caps_shift: 'ф', code: 'KeyA',
+},
+{
+  key: 'ы', key_shift: 'Ы', key_caps: 'Ы', key_caps_shift: 'ы', code: 'KeyS',
+},
+{
+  key: 'в', key_shift: 'В', key_caps: 'В', key_caps_shift: 'в', code: 'KeyD',
+},
+{
+  key: 'а', key_shift: 'А', key_caps: 'А', key_caps_shift: 'а', code: 'KeyF',
+},
+{
+  key: 'п', key_shift: 'П', key_caps: 'П', key_caps_shift: 'п', code: 'KeyG',
+},
+{
+  key: 'р', key_shift: 'Р', key_caps: 'Р', key_caps_shift: 'р', code: 'KeyH',
+},
+{
+  key: 'о', key_shift: 'О', key_caps: 'О', key_caps_shift: 'о', code: 'KeyJ',
+},
+{
+  key: 'л', key_shift: 'Л', key_caps: 'Л', key_caps_shift: 'л', code: 'KeyK',
+},
+{
+  key: 'д', key_shift: 'Д', key_caps: 'Д', key_caps_shift: 'д', code: 'KeyL',
+},
+{
+  key: 'ж', key_shift: 'Ж', key_caps: 'ж', key_caps_shift: 'Ж', code: 'Semicolon',
+},
+{
+  key: 'э', key_shift: 'Э', key_caps: 'э', key_caps_shift: 'Э', code: 'Quote',
+},
+{
+  key: 'Enter', key_shift: 'Enter', key_caps: 'Enter', key_caps_shift: 'Enter', code: 'Enter',
+},
+{
+  key: 'Shift', key_shift: 'Shift', key_caps: 'Shift', key_caps_shift: 'Shift', code: 'ShiftLeft',
+},
+{
+  key: 'я', key_shift: 'Я', key_caps: 'Я', key_caps_shift: 'я', code: 'KeyZ',
+},
+{
+  key: 'ч', key_shift: 'Ч', key_caps: 'Ч', key_caps_shift: 'ч', code: 'KeyX',
+},
+{
+  key: 'с', key_shift: 'С', key_caps: 'С', key_caps_shift: 'с', code: 'KeyC',
+},
+{
+  key: 'м', key_shift: 'М', key_caps: 'М', key_caps_shift: 'м', code: 'KeyV',
+},
+{
+  key: 'и', key_shift: 'И', key_caps: 'И', key_caps_shift: 'и', code: 'KeyB',
+},
+{
+  key: 'т', key_shift: 'Т', key_caps: 'Т', key_caps_shift: 'т', code: 'KeyN',
+},
+{
+  key: 'ь', key_shift: 'Ь', key_caps: 'Ь', key_caps_shift: 'ь', code: 'KeyM',
+},
+{
+  key: 'б', key_shift: 'Б', key_caps: 'б', key_caps_shift: 'Б', code: 'Comma',
+},
+{
+  key: 'ю', key_shift: 'Ю', key_caps: 'ю', key_caps_shift: 'Ю', code: 'Period',
+},
+{
+  key: '.', key_shift: ',', key_caps: '.', key_caps_shift: ',', code: 'Slash',
+},
+{
+  key: '↑', key_shift: '↑', key_caps: '↑', key_caps_shift: '↑', code: 'ArrowUp',
+},
+{
+  key: 'Shift', key_shift: 'Shift', key_caps: 'Shift', key_caps_shift: 'Shift', code: 'ShiftRight',
+},
+{
+  key: 'Control', key_shift: 'Control', key_caps: 'Control', key_caps_shift: 'Control', code: 'ControlLeft',
+},
+{
+  key: 'Alt', key_shift: 'Alt', key_caps: 'Alt', key_caps_shift: 'Alt', code: 'AltLeft',
+},
+{
+  key: 'Meta', key_shift: 'Meta', key_caps: 'Meta', key_caps_shift: 'Meta', code: 'MetaLeft',
+},
+{
+  key: ' ', key_shift: ' ', key_caps: ' ', key_caps_shift: ' ', code: 'Space',
+},
+{
+  key: 'Meta', key_shift: 'Meta', key_caps: 'Meta', key_caps_shift: 'Meta', code: 'MetaRight',
+},
+{
+  key: 'Alt', key_shift: 'Alt', key_caps: 'Alt', key_caps_shift: 'Alt', code: 'AltRight',
+},
+{
+  key: '←', key_shift: '←', key_caps: '←', key_caps_shift: '←', code: 'ArrowLeft',
+},
+{
+  key: '↓', key_shift: '↓', key_caps: '↓', key_caps_shift: '↓', code: 'ArrowDown',
+},
+{
+  key: '→', key_shift: '→', key_caps: '→', key_caps_shift: '→', code: 'ArrowRight',
+},
+{
+  key: 'Control', key_shift: 'Control', key_caps: 'Control', key_caps_shift: 'Control', code: 'ControlRight',
+}];
 export { eng, rus };
